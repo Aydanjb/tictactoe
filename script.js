cd455298cf873255790ff0a6d0e20019b6fe04a7
aydan <AydanBondurant@gmail.com>
2023-03-06 16:52:55 -0500

    added player and winning/drawing bitboards

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ const Player = (token) => {
 };
 
 const gameBoard = (() => {
+  let xBoard = 0b000_000_000;
+  let oBoard = 0b000_000_000;
 
+
+
+  return {};
 })();
 
 const displayController = (() => {
@@ -17,5 +22,16 @@ const displayController = (() => {
 })();
 
 const gameController = (() => {
-
+  const fullBoard = 0b111_111_111;
+  const winningBoards = [
+    0b000_000_111,
+    0b000_111_000,
+    0b111_000_000, // 3 rows
+    0b001_001_001,
+    0b010_010_010,
+    0b100_100_100, // 3 columns
+    0b100_010_001,
+    0b001_010_100, // 2 diagonals
+  ];
+  
 })();
